aaef46258e34c877a23727fe4c084bb75e95473d
Andrey <Andrey@ANDREY-PC>
2016-09-24 21:07:59 +0300

    Ready for release

--- FileBrowsingWebSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FileBrowsingWebSystem.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FileBrowsingWebSystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FileBrowsingWebSystem.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
